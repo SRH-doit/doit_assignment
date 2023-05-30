6b7874121f78209a0541ae3c6a0b03fabe03dc03
dnjeh(Notebook) <ehdnj.001@gmail.com>
2023-05-30 18:38:59 +0900

    Today's C and V!

---
+++ 김도원/importeddnjeh.web/11/01/index.js
@@ -0,0 +1,8 @@
+// 아래에 코드를 작성해 주세요.
+
+document.write("hi");
+
+const age = 30;
+const nickName = "Elice";
+
+document.write(`저의 이름은 ${nickName}이고 나이는 ${age}입니다.`);
---
+++ 김도원/importeddnjeh.web/11/02/index.js
@@ -0,0 +1,9 @@
+// 아래에 코드를 작성해 주세요.
+
+alert("경고창을 열어봅니다.");
+const name = prompt("너의 이름은", "");
+document.write(name);
+
+const bool = confirm("yes or no?");
+document.write(bool);
+console.log("hello javascript");
