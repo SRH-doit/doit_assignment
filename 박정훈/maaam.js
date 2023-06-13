60ee1c1b0e4500d2dea8eb79d5153df6f964c549
monlaoi002 <qkrwjdgns270@gmail.com>
2023-06-13 18:35:04 +0900

    mmm
    lllk

---
+++ 박정훈/maaam.js
@@ -0,0 +1,8 @@
+//alert('경고창을 열어봅니다.');
+//const llll = prompt('너의 이름은', '');
+//document.write(llll);
+
+//const bool = confirm('yes or no?');
+//document.write(bool);
+
+//console.log('hello javascript');
